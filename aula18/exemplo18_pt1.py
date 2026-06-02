3cd1d425da13bf5b35d8e148315a16cc6eae9432
abreuadrian <abreuadrianoliv@gmail.com>
2026-06-02 19:35:17 -0300

    feat: add .env and .gitignore

--- aula18/exemplo18_pt1.py
+++ aula18/exemplo18_pt1.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 import pandas as pd 
+import os 
 
-host = 'localhost'
-user = 'root'
-password = ''
-database = 'db_aula18'
+load_dotenv()
+host = os.getenv('DB_host')
+user = os.getenv('DB_user')
+password = os.getenv('DB_password')
+database = os.getenv('DB_database')
 
 engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{database}')
